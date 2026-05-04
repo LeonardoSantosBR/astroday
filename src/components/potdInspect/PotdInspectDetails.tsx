34c21153146d991d2dfefa6fc3deb6f8069b8ee2
Leonardo Santos <leosantosmelo07@gmail.com>
2026-05-04 16:36:36 -0300

    refactor: remove padding from PotdInspectDetails component for improved layout

--- src/components/potdInspect/PotdInspectDetails.tsx
+++ src/components/potdInspect/PotdInspectDetails.tsx
@@ -4,7 +4,7 @@ import { PotdInspectMediaPreview } from "./PotdInspectMediaPreview";
 
 export default function PotdInspectDetails(data: potdDataType): React.JSX.Element {
   return (
-    <View className="px-4 pb-8 gap-3">
+    <View className="gap-3">
       <PotdInspectMediaPreview url={data?.url} media_type={data?.media_type} />
       <View className="gap-1">
         <Text className="text-text-primary font-bold text-xl leading-snug">
